8808fb397f018aeaef731afe867aaec340ce3df0
Ian Scott Martin <ianscottmartin@gmail.com>
2023-03-25 18:58:07 -0700

    manning baby

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+
+superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W");
+    return !!result ? result.year : undefined;
+
+}
+//find((element, index, array) => { /* … */ })
+
